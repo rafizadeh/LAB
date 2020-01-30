8907ff7c365df189fae855218f3b3120c8d7c4ee
rafizadeh <42311158+rafizadeh@users.noreply.github.com>
2020-01-31 02:06:39 +0400

    car app

---
+++ 12. 30.01.2020/30012020/Car.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30012020
+{
+    class Car
+    {
+        // 0'lana bilen probeg
+        private double _localKm;
+        public double LocalKm
+        {
+            get
+            {
+                return _localKm;
+            }
+        }
+        // 0'lanmayan probeg
+        private double _globalKm;
+        public double GlobalKm
+        {
+            get
+            {
+                return _globalKm;
+            }
+        }
+        // hazirda bakda olan yanacaq miqdari
+        private double _currentFuel;
+
+        //Umumi bakin hecmi
+        private double _fuelCapacity;
+
+        // 100 km'e serf edilen yanacaq
+        private double _fuelUsage;
+
+        //bakdaki yanacaqla gedile bilecek mesafe
+        private double _maxLimitKm;
+        public double MaxLimit
+        {
+            get
+            {
+                return _maxLimitKm;
+            }
+        }
+
+        /// <summary>
+        ///  Constructora bakin hecmi ve 100 km'e ne qeder serf edeceyi miqdari gonderirik
+        ///  ve Car yaradilanda uzerinde kapasitesinin 10%'i ile gelir
+        /// </summary>
+        public Car(double FuelCapacity, double FuelUsage)
+        {
+            _fuelCapacity = FuelCapacity;
+            _fuelUsage = FuelUsage;
+
+            _currentFuel = _fuelCapacity * 0.1;
+
+            CalcMaxLimit();
+        }
+
+        // Max km'ni hesablayan metod
+        private double CalcMaxLimit()
+        {
+            return _maxLimitKm = _currentFuel / _fuelUsage * 100;
+        }
+
+        public bool Drive(double km)
+        {
+            if(km > _maxLimitKm)
+            {
+                return false;
+            }
+
+            _localKm += km;
+            _globalKm += km;
+
+            _currentFuel -= km / 100 * _fuelUsage;
+            CalcMaxLimit();
+            return true;
+        }
+
+        public bool AddFuel(double litr)
+        {
+            if(_currentFuel + litr > _fuelCapacity)
+            {
+                return false;
+            }
+
+            _currentFuel += litr;
+
+            CalcMaxLimit();
+            return true;
+        }
+        // local km 0lanir
+        public void ResetLocal()
+        {
+            _localKm = 0;
+        }
+
+
+        public double NeededFuel()
+        {
+            return _fuelCapacity - _currentFuel;
+        }
+
+    }
+}
---
+++ 12. 30.01.2020/30012020/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30012020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(100,5);
+
+            Console.WriteLine("Welcome to the Car Application");
+            Console.WriteLine("*******************************");
+            int select = 0;
+            string selectedStr;
+
+
+            do
+            {
+                Console.WriteLine("1. Sur");
+                Console.WriteLine("2. Yanacaq elave et");
+                Console.WriteLine("3. Local km");
+                Console.WriteLine("4. Global km");
+                Console.WriteLine("0. Cixis");
+
+                selectedStr = Console.ReadLine();
+                select = Convert.ToInt32(selectedStr);
+
+
+                switch (select)
+                {
+                    case 1:
+                        Console.WriteLine("*******************************");
+                        Console.WriteLine("Surmek istediyiniz km yazin:");
+                        string kmStr = Console.ReadLine();
+                        double km;
+
+                        while(!double.TryParse(kmStr, out km) || km <= 0)
+                        {
+                            Console.WriteLine("Duzgun km daxil edin!!");
+                            kmStr = Console.ReadLine();
+                        }
+                        if (car.Drive(km))
+                        {
+                            Console.WriteLine("Siz mashini {0} km surdunuz, bundan sonra {1} km sure bilersiniz ",km, car.MaxLimit);
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0} km sure bilmezsiniz, chunki {1} km gede bilersiniz", km, car.MaxLimit);
+                            kmStr = Console.ReadLine();
+                        }
+
+
+                        break;
+                    case 2:
+                        Console.WriteLine("*******************************");
+                        Console.WriteLine("Elave etmek istediyiniz litri yazin:");
+                        string litrStr = Console.ReadLine();
+                        double litr;
+
+                        while (!double.TryParse(litrStr, out litr) || litr < 0)
+                        {
+                            Console.WriteLine("Litri duzgun daxil edin!");
+                            litrStr = Console.ReadLine();
+                        }
+                        if (car.AddFuel(litr))
+                        {
+                            Console.WriteLine("Siz {0} litr yanacaq elave etdiniz, bundan sonra {1} km sure bilersiniz", litr, car.MaxLimit);
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0} litr elave ede bilmezsiniz chunki {1} litr kapasite qalib", litr, car.NeededFuel());
+                            litrStr = Console.ReadLine();
+                        }
+
+
+                        break;
+                    case 3:
+                        Console.WriteLine("*******************************");
+                        Console.WriteLine("Local km: {0}km ", car.LocalKm);
+                        Console.WriteLine("Local km'i 0'lamaq isteyirsiniz mi?(y/n)");
+                        string s = Console.ReadLine();
+
+                        if(s.ToLower() != "y" && s.ToLower() != "n")
+                        {
+                            Console.WriteLine("Adam ol, sechim yalniz y/n ola biler!");
+                        }
+                        else if(s.ToLower() == "y")
+                        {
+                            car.ResetLocal();
+                            Console.WriteLine("Local km sifirlandi.");
+                        }
+
+                        break;
+                    case 4:
+                        Console.WriteLine("*******************************");
+                        Console.WriteLine("Global km: {0}", car.GlobalKm);
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("*******************************");
+                        Console.WriteLine("Sechiminizi duzgun daxil edin!");
+                        break;
+                }
+
+            } while (select != 0);
+
+        }
+    }
+}
