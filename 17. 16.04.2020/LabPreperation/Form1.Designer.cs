24955071e2d62dcb38478c72ecdec0d8bfdab2bb
rafizadeh <42311158+rafizadeh@users.noreply.github.com>
2020-04-16 21:03:46 +0400

    WinFortask added

---
+++ 17. 16.04.2020/LabPreperation/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace LabPreperation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbGroups = new System.Windows.Forms.ComboBox();
+            this.cmbStudents = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.AllowUserToAddRows = false;
+            this.dgvResults.AllowUserToDeleteRows = false;
+            this.dgvResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvResults.BackgroundColor = System.Drawing.Color.DarkGray;
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column4,
+            this.Column3,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dgvResults.Location = new System.Drawing.Point(12, 150);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.RowTemplate.Height = 24;
+            this.dgvResults.Size = new System.Drawing.Size(776, 288);
+            this.dgvResults.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            this.Column1.Visible = false;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Qrup";
+            this.Column2.Name = "Column2";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Tələbə";
+            this.Column4.Name = "Column4";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Imtahan";
+            this.Column3.Name = "Column3";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Tarix";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Qiymət";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Nəticə";
+            this.Column7.Name = "Column7";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Qrup";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tələbə";
+            // 
+            // cmbGroups
+            // 
+            this.cmbGroups.FormattingEnabled = true;
+            this.cmbGroups.Location = new System.Drawing.Point(16, 62);
+            this.cmbGroups.Name = "cmbGroups";
+            this.cmbGroups.Size = new System.Drawing.Size(143, 24);
+            this.cmbGroups.TabIndex = 5;
+            this.cmbGroups.SelectedIndexChanged += new System.EventHandler(this.cmbGroups_SelectedIndexChanged);
+            // 
+            // cmbStudents
+            // 
+            this.cmbStudents.FormattingEnabled = true;
+            this.cmbStudents.Location = new System.Drawing.Point(264, 62);
+            this.cmbStudents.Name = "cmbStudents";
+            this.cmbStudents.Size = new System.Drawing.Size(143, 24);
+            this.cmbStudents.TabIndex = 6;
+            this.cmbStudents.SelectedIndexChanged += new System.EventHandler(this.cmbStudents_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbStudents);
+            this.Controls.Add(this.cmbGroups);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvResults);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.ComboBox cmbGroups;
+        private System.Windows.Forms.ComboBox cmbStudents;
+    }
+}
+
---
+++ 17. 16.04.2020/LabPreperation/Form1.cs
@@ -0,0 +1,109 @@
+﻿using LabPreperation.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabPreperation
+{
+    public partial class Form1 : Form
+    {
+
+        private P507Entities1 db = new P507Entities1();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            FillGroups();
+
+        }
+        // qrupun id'sini saxlamaq üçün dəyişən
+        public int selectedGroupId { get; set; }
+
+
+        // Qrupları qrup comboboxuna dolduran metod
+        private void FillGroups()
+        {
+            cmbGroups.Items.Add("Hamısı");
+            cmbGroups.Text = "Hamısı";
+
+            List<Group> groups = db.Groups.ToList();
+
+            foreach (Group group in groups)
+            {
+                cmbGroups.Items.Add(group.Name);
+            }
+        }
+
+
+        // bütün nəticələri datagridview'ya doldurmaq üçün metod
+        private void FillResults(int GroupId = 0, int StudentId = 0)
+        {
+            List<Result> results = db.Results.Where(r => (GroupId != 0 ? r.Student.GroupId == GroupId : true) && (StudentId != 0 ? r.StudentId == StudentId : true)).OrderByDescending(r => r.Date).ToList();
+
+            dgvResults.Rows.Clear();
+            foreach (Result result in results)
+            {
+                dgvResults.Rows.Add(result.Id,
+                                    result.Student.Group.Name,
+                                    result.Student.Name + " " + result.Student.Surname,
+                                    result.Exam.Name,
+                                    result.Date.ToString("dd.MM.yyyy"),
+                                    result.ExamResult.ToString("#.##"),
+                                    result.ExamResult > 700 ? "Uğurlu":"Qaldı");
+            }
+
+        }
+
+        private void cmbGroups_SelectedIndexChanged(object sender, EventArgs e)
+        {      
+            if(cmbGroups.Text != "Hamısı")
+            {
+                Group group = db.Groups.FirstOrDefault(g => g.Name == cmbGroups.Text);
+                cmbStudents.Items.Clear();
+
+                // select * from Students where groupId = 2
+
+                //List<Student> students = db.Students.Where(s => s.GroupId == group.Id).ToList();
+
+                cmbStudents.Items.Add("Hamısı");
+                cmbStudents.Text = "Hamısı";
+
+                selectedGroupId = group.Id;
+                foreach (Student student in group.Students.ToList())
+                {
+                    cmbStudents.Items.Add(student.Name +" "+student.Surname);
+                }
+
+                FillResults(group.Id);
+            }
+            else
+            {
+                FillResults();
+                cmbStudents.Items.Clear();
+                cmbStudents.Text = "";
+            }
+        }
+
+        private void cmbStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cmbStudents.Text != "Hamısı")
+            {
+                Student stu = db.Students.FirstOrDefault(s => s.Name + " " + s.Surname == cmbStudents.Text);
+                FillResults(StudentId: stu.Id);
+
+            }
+            else
+            {
+                FillResults(selectedGroupId);   
+            }
+
+        }
+    }
+}
