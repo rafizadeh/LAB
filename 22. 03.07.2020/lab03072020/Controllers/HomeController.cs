a480e7dbfa3b7b0325771ed24cf6f7bfc8062208
rafizadeh <42311158+rafizadeh@users.noreply.github.com>
2020-07-03 21:47:25 +0400

    SweetAlert SendEmail

---
+++ 22. 03.07.2020/lab03072020/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using lab03072020.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace lab03072020.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult SendMail(Client client)
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("p114code@gmail.com", "Group P114");
+            mail.To.Add(new MailAddress(client.Email));
+
+            mail.Subject = client.Subject;
+            mail.CC.Add(new MailAddress(client.CC));
+            mail.Body = $"<h2 style='color:red;'>test email to {client.Name} {client.Surname}</h2> <p>{client.Message}</p>";
+            mail.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+
+            smtp.Credentials = new NetworkCredential("p114code@gmail.com", "123456p114");
+            smtp.Send(mail);
+
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ 22. 03.07.2020/lab03072020/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab03072020.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string CC { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
